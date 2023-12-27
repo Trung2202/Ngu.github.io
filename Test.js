3578ada4522fc2097801a98dd128162353547aae
Trung2202 <146946024+Trung2202@users.noreply.github.com>
2023-12-27 19:04:02 +0700

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,11 @@
+function mouseOver() {
+    document.getElementById('myDiv').innerHTML = 'Có';
+    }
+    function mouseOut() {
+    document.getElementById('myDiv').innerHTML = 'Không';
+    }
+
+function bam() {
+    alert('Bạn bị ngu')
+    
+}
